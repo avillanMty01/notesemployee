044cdb4c40b200c981b677dd2611c0ae70167413
Alfonso Villanueva <avillanMty01@yahoo.com>
2021-03-10 17:19:11 -0600

    starting test with domino json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const myCounter = 999
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>My Counter</p>
+      <span>{myCounter}</span>
     </div>
   );
 }
